e109d777fe0efae8e898284f327e00b8e14ab942
Eilon Toledano <eilon.toledano@tandemg.com>
2022-04-19 15:23:44 +0300

    declare and wire up processes

--- DUT/top.vhd
+++ DUT/top.vhd
@@ -100,9 +100,69 @@ entity top is
 end top;
 ------------- complete the top Architecture code --------------
 architecture arc_sys of top is
+	------------- component declare --------------
+	------------- process1 --------------
+		component process1 is
+			port(
+				rst,ena,clk : in std_logic;
+				x : in std_logic_vector(n-1 downto 0);
+				x_previous1, x_previous2 : out std_logic_vector(n-1 downto 0)
+			);
+		end component;
+		------------- process2 --------------
+		component process2 is
+			port(
+				rst,ena,clk : in std_logic;
+				x_previous1, x_previous2 : in std_logic_vector(n-1 downto 0);
+				DetectionCode : in integer range 0 to 3;
+				valid : out std_logic
+			);
+		end component;
+		------------- process3 --------------
+		component process3 is
+			port(
+				rst,ena,clk : in std_logic;
+				valid : out std_logic;
+				DetectionCode : in integer range 0 to 3;
+				detector : out std_logic
+			);
+		end component;
+
+------------- signals declare --------------
+SIGNAL x_previous1, x_previous2	: STD_LOGIC_VECTOR(n-1 downto 0);
+SIGNAL valid	: STD_LOGIC;
 
 begin
 
+------------- wire up processes --------------
+	------------- process1 --------------
+    process1_pm: process1 port map(
+        rst => rst,
+        ena => ena,
+        clk => clk,
+        x => x,
+        x_previous1 => x_previous1,
+        x_previous2 => x_previous2
+    );
+	------------- process2 --------------
+    process2_pm: process2 port map(
+        rst => rst,
+        ena => ena,
+        clk => clk,
+        x_previous1 => x_previous1,
+        x_previous2 => x_previous2,
+        DetectionCode => DetectionCode,
+        valid => valid
+    );
+	------------- process3 --------------
+    process3_pm: process3 port map(
+        rst => rst,
+        ena => ena,
+        clk => clk,
+        DetectionCode => DetectionCode,
+        valid => valid,
+        detector => detector
+    );
 
 end arc_sys;
 

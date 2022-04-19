b865ca3ceffc8cfc9ab510f4436689993ba40ae9
Barry <bary_yosi@hotmail.com>
2022-04-19 18:01:40 +0300

    updated 3rd process

--- DUT/top.vhd
+++ DUT/top.vhd
@@ -19,32 +19,62 @@ end top;
 ------------- complete the top Architecture code --------------
 architecture arc_sys of top is
 	SIGNAL x_previous1, x_previous2: STD_LOGIC_VECTOR (n-1 DOWNTO 0);
+	SIGNAL validCounter : INTEGER range 0 to m;
+	BEGIN
 
-	PROCESS (x, rest, clk, ena)
+	PROCESS (x, rst, clk, ena)
 
 		BEGIN
 			IF (ena = '0')	THEN
 				x_previous2 <= x_previous2;
 				x_previous1 <= x_previous1;
 			ELSIF (rst = '1') THEN
-				x_previous2 <= <= (others => '0');
-				x_previous1 <= <= (others => '0');
+				x_previous2 <= (others => '0');
+				x_previous1 <= (others => '0');
 			ELSIF (rising_edge(clk)) THEN
 				x_previous2 <= x_previous1;
 				x_previous1 <= x;
 			END IF;
 		END PROCESS;
-					
-
-
-
-begin
 	
+	
+
+	PROCESS(valid, rst, clk, ena, detector)
+		
+		BEGIN
+			IF (ena = '0') THEN
+				validCounter <= validCounter;
+				IF (validCounter < m) THEN
+					detector <= '0';
+				ELSE 
+					detector <= '1';
+				END IF;
+			ELSE IF (rst = '1') THEN
 				
+				validCounter <= 0;
+				detector <= '0';
+			
+			ELSIF (rising_edge(clk)) THEN
+
+				IF(valid = '1') THEN
+					validCounter <= validCounter + 1;
+				ELSE
+					validCounter <= 0;
+				END IF;
+
+				IF (validCounter < m) THEN
+					detector <= '0';
+				ELSE 
+					detector <= '1';
+				END IF;
+			END IF;
+
+
+		END PROCESS;
 	
 	
 	
-end arc_sys;
+	END arc_sys;
 
 
 
